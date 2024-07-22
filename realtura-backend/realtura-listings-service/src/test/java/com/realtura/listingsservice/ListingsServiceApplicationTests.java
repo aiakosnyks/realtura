e951d28f07afca29b4aca40fa5797db3ca27a62c
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2024-07-22 08:01:43 +0300

    Bug fix

--- realtura-backend/realtura-listings-service/src/test/java/com/realtura/listingsservice/ListingsServiceApplicationTests.java
+++ realtura-backend/realtura-listings-service/src/test/java/com/realtura/listingsservice/ListingsServiceApplicationTests.java
@@ -1,111 +1,68 @@
 package com.realtura.listingsservice;
 
-import com.realtura.listingsservice.dto.request.ListingDeleteRequest;
+import com.realtura.listingsservice.controller.ListingController;
+import com.realtura.listingsservice.converter.ListingConverter;
 import com.realtura.listingsservice.dto.request.ListingSaveRequest;
-import com.realtura.listingsservice.dto.request.ListingSearchRequest;
 import com.realtura.listingsservice.dto.response.CreateResponse;
 import com.realtura.listingsservice.dto.response.GenericResponse;
-import com.realtura.listingsservice.dto.response.ListingResponse;
+import com.realtura.listingsservice.model.Address;
 import com.realtura.listingsservice.model.Listing;
-import com.realtura.listingsservice.repository.ListingRepository;
+import com.realtura.listingsservice.model.enums.HeatingType;
+import com.realtura.listingsservice.model.enums.ListingType;
 import com.realtura.listingsservice.service.ListingService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ListingsServiceApplicationTests {
-    @Mock
-    private ListingRepository listingRepository;
+public class ListingsServiceApplicationTests {
 
-    @InjectMocks
+    @Autowired
+    private ListingController listingController;
+
+    @Autowired
     private ListingService listingService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+    @Autowired
+    private ListingService subscriptionService;
+    @MockBean
+    private ListingConverter listingConverter;
 
     @Test
-    void save() {
+    public void testSaveListingSuccess() {
+
         ListingSaveRequest request = new ListingSaveRequest();
-        Listing listing = new Listing();
-        listing.setId(1L);
-
-        when(listingRepository.save(any(Listing.class))).thenReturn(listing);
-
-        GenericResponse<?> response = listingService.save(request);
-
-        assertNotNull(response);
-        assertTrue(response.getStatus().equals("SUCCESS"));
-        assertEquals(HttpStatus.OK, response.getHttpStatus());
-      //  assertEquals(1L, response.getData().getId());
+        request.setAddress(new Address());
+        request.setBuiltIn(1);
+        request.setBedroom(1);
+        request.setBathroom(1);
+        request.setNetArea(1);
+        request.setGrossArea(1);
+        request.setHeatingType(HeatingType.HEAT_PUMPS);
+        request.setListingType(ListingType.SALE);
+        request.setPhoto("test");
+        request.setPrice(1);
+        request.setTitle("test");
+        request.setDescription("test");
+        request.setUserId(1L);
+        ResponseEntity<GenericResponse<?>> response = listingController.save(request);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(response.getBody().getStatus().equals("SUCCESS"));
     }
-
     @Test
-    void getAllByFilter() {
-        ListingSearchRequest request = new ListingSearchRequest();
-        request.setPage(0);  // Set a valid page number
-        request.setSize(10); // Set a valid page size
-        PageRequest pageRequest = PageRequest.of(0, 10);
-        Listing listing = new Listing();
-        Page<Listing> listingPage = new PageImpl<>(List.of(listing));
+    public void testSaveListingFail() {
 
-        when(listingRepository.findAll(any(Specification.class), any(PageRequest.class))).thenReturn(listingPage);
-
-        List<ListingResponse> responses = listingService.getAllByFilter(request);
-
-        assertNotNull(responses);
-        assertEquals(1, responses.size());
-    }
-
-    @Test
-    void update() {
-        Long id = 1L;
-        ListingSaveRequest request = new ListingSaveRequest();
-        Listing listing = new Listing();
-        listing.setId(id);
-
-        when(listingRepository.findById(id)).thenReturn(Optional.of(listing));
-        when(listingRepository.save(any(Listing.class))).thenReturn(listing);
-
-        GenericResponse<?> response = listingService.update(id, request);
-
-        assertNotNull(response);
-        assertTrue(response.getStatus().equals("SUCCESS"));
-        assertEquals(HttpStatus.OK, response.getHttpStatus());
-        assertEquals("subscriptions updated", response.getMessage());
-    }
-
-    @Test
-    void delete() {
-        ListingDeleteRequest request = new ListingDeleteRequest();
-        request.setId(1L);
-        Listing listing = new Listing();
-
-        when(listingRepository.findAll(any(Specification.class))).thenReturn(List.of(listing));
-
-        GenericResponse<?> response = listingService.delete(request);
-
-        verify(listingRepository, times(1)).deleteById(request.getId());
-        assertTrue(response.getStatus().equals("SUCCESS"));
-        assertEquals(HttpStatus.OK, response.getHttpStatus());
-        assertEquals("subscriptions deleted", response.getMessage());
     }
 }
